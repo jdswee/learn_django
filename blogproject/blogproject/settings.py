c0c8ab2419dc3dda8d2b32f0457b6ed1f5734ef6
LiQiang <jdswee@163.com>
2018-05-22 12:10:32 +0800

    NEW 部署前的项目配置

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mjmqwon_7$ng_z0e_fa^1n6ps=m!xc_1gfnpx7-9)%*^n2)45z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.leeqiang.site']
 
 # Application definition
 
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
